081b6d0b4436a25d7ec387834d2bae7dc171933f
mars <zhiming137@gmail.com>
2020-01-09 10:40:52 +0800

    添加header获取

--- longhu/longhu.cookie.js
+++ longhu/longhu.cookie.js
@@ -4,9 +4,10 @@ const tokenVal = $request.headers['token']
 const userKey = 'my_userkey_longhu'
 const userKeyVal = $request.headers['userKey']
 
-if (tokenVal) {
+if (tokenVal && userKeyVal) {
   let token = $persistentStore.write(tokenVal, tokenKey)
   let userKey = $persistentStore.write(userKey, userKeyVal)
+
   if (token && userKey) {
     let msg = `${cookieName}`
     $notification.post(msg, 'token写入成功', '详见日志')
